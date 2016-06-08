10f194d293b196521bf6a5a70221a210f5c60fae
Zesheng Zong <zesheng.zong@gmail.com>
2016-06-08 16:35:03 -0400

    Set up route of views, add common attributes in common.js file

--- js/src/app.js
+++ js/src/app.js
@@ -2,45 +2,13 @@
   'jquery',
   'underscore',
   'backbone',
-  './views/post-mortem',
-  './views/workflows'
-], function ($, _, Backbone, PostMortemView, WorkflowsView) {
-
-  function init () {
-    Backbone.siteRootUrl = $('#site-root-url').attr('data-root-url');
-
-    var incident = {
-      incidentId: Number($("#IncidentId").val()),
-      workflowTypeId: Number($("#WorkflowTypeId").val())
-    };
-
-    var $infoTag = $('#visit-user-info');
-    var user = {
-      name: $infoTag.attr('data-username'),
-      emailAddress: $infoTag.attr('data-email-address')
-    }
-    Backbone.incident = incident;
-    Backbone.user = user;
-  }
-
-  function route() {
-    var href = window.location.href.toLowerCase();
-
-    if (href.indexOf('postmortem') >= 0) {
-      var postMortem = new PostMortemView();
-      postMortem.render();
-    } else if (href.indexOf('participant') >= 0) {
-      var workflows = new WorkflowsView();
-      workflows.render();
-    }
-  }
+  './views/post-mortem'
+], function ($, _, Backbone, PostMortemView) {
 
   var initialize = function () {
-    init();
-    route();
+    var postMortemView = new PostMortemView();
+    postMortemView.render();
   };
-
-  
 
   return {
     initialize: initialize
