3547f1afb89e32e4d7a566d2c22c0f68d260d47d
Zesheng Zong <zesheng.zong@gmail.com>
2016-06-08 16:38:27 -0400

    Add confirm popout before re-activate an incident

--- js/src/views/post-mortem.js
+++ js/src/views/post-mortem.js
@@ -2,24 +2,14 @@
   'jquery',
   'underscore',
   'backbone',
-  '../common',
+  'velocity',
   './components/navigation',
-  './next-workflow-popout',
-  './email-popout',
-  '../utils/email-service',
-  '../utils/animation',
-], function (
-  $, _, Backbone,
-  common,
-  Navigation,
-  NextWorkflowPopoutView,
-  EmailPopoutView,
-  EmailService,
-  Animation) {
+  './components/progress-bar'
+], function ($, _, Backbone, velocity, Navigation, ProgressBar) {
 
   PostMortemView = Backbone.View.extend({
 
-    el: '#post-mortem-app',
+    el: $(window),
 
     navTags: [
       { tagId: '#overview', name: 'Overview' },
@@ -30,61 +20,38 @@
     ],
 
     events: {
-      'click .backtop-btn'    : 'scrollToTop',
-      'click #show-popout-btn': 'showPopout',
-      'click .edit-btn'       : 'submitEditForm',
-      'click .email-reminder-btn': 'displayEmailPopout',
-      'click .email-lastcall-btn': 'displayEmailPopout',
-      'click #show-status-details-btn': 'showStagesDetail',
-      'click .close-status-detail-btn': 'hideStagesDetail',
-      
+      'click #backtop-btn': 'scrollToTop',
+      'scroll': 'toggleComponents',
     },
 
     initialize: function () {
-      var toggleComponents = this.toggleComponents.bind(this);
-      this.pageNav = new Navigation({ model: this.navTags });
-      this.submitPopout = new NextWorkflowPopoutView();
-      this.$showStagesDetailBtn = $('#show-status-details-btn');
-      $(window).scroll(toggleComponents)
 
-      $(".case-status-text").hover(
-      	function() {
-      	  $dropdown = $(this).find(".case-status-detail-wrapper");
-      	  $dropdown.show();
-      		var width = $dropdown.width();
-      		var defaultOffsetLeft = $(document).width() - width - 100;
-      		var offset = $dropdown.offset();
-      		if (offset != void 0 && offset.left > defaultOffsetLeft) {
-      		  $dropdown.offset({ left: defaultOffsetLeft });
-      		}
-      	},
-      	function() {
-      		$(this).find(".case-status-detail-wrapper").hide();
-      	}
-      );
+      this.pageNav = new Navigation({ model: this.navTags });
+      this.progressBar = new ProgressBar();
     },
 
     render: function () {
       this.pageNav.render();
-    },
-
-    submitEditForm: function() {
-      $('#EditForm').submit();
+      this.progressBar.render();
     },
 
     scrollToTop: function () {
-      var el = this.el;
-      Animation.scrollTo(el, { duration: 600, offset: -100, easing: 'ease-in-out' });
+      this.$el.velocity('scroll', {
+        duration: 500,
+        offset: -100,
+        easing: 'ease-in-out'
+      })
     },
 
     toggleComponents: function () {
       this.toggleSideMenu();
+
     },
 
     toggleSideMenu: function () {
       var $backtopBtn = $('#backtop-btn-container'),
-          $pageNav = $('#side-nav-container'),
-          scrollHeight = $(window).scrollTop();
+          $pageNav = $('#page-nav'),
+          scrollHeight = this.$el.scrollTop();
       if (scrollHeight > 300) {
         $backtopBtn.fadeIn(400);
         $pageNav.addClass('active');
@@ -94,46 +61,8 @@
       }
     },
 
-    showPopout: function () {
-      this.submitPopout.render();
-    },
 
-    displayEmailPopout: function (e) {
-      var incidentId = Backbone.incident.incidentId,
-          workflowTypeId = Backbone.incident.workflowTypeId,
-          emailService, emailPopout;
-        
-      if (Backbone.incident.workflowTypeId === common.WORKFLOW_TYPE.WorkingGroup) {
-        emailService = this._getEmailService('email', 'ComposeLastCallEmail');
-      } else {
-        emailService = this._getEmailService('email', 'ComposeReminderEmail');
-      }
-      
-      this.emailPopout = new EmailPopoutView({
-        model: {
-          emailService: emailService,
-          data: {
-            incidentId: incidentId,
-            workflowType: workflowTypeId
-          }
-        }
-      });
-    },
 
-    _getEmailService: function (controller, action) {
-      return new EmailService(controller, action);
-    },
-
-    showStagesDetail: function () {
-      var $btn = this.$showStagesDetailBtn;
-      Animation.toggle($btn, '#status-detail-toggle-wrapper');
-    },
-
-    hideStagesDetail: function (e) {
-      var $btn = this.$showStagesDetailBtn;
-      $btn.trigger('click');
-      $('#close-status-detail-container').css('top', -20);
-    },
 
   });
 
