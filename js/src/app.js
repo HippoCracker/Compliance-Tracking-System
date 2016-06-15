4c5088374088770e9923a8c038afcb97ce94984b
Zesheng Zong <zesheng.zong@gmail.com>
2016-06-15 13:03:25 -0400

    Add simple route in app.js file to initialize difference Views base on url

--- js/src/app.js
+++ js/src/app.js
@@ -2,13 +2,45 @@
   'jquery',
   'underscore',
   'backbone',
-  './views/post-mortem'
-], function ($, _, Backbone, PostMortemView) {
+  './views/post-mortem',
+  './views/workflows'
+], function ($, _, Backbone, PostMortemView, WorkflowsView) {
+
+  function init () {
+    Backbone.siteRootUrl = $('#site-root-url').attr('data-root-url');
+
+    var incident = {
+      incidentId: Number($("#IncidentId").val()),
+      workflowTypeId: Number($("#WorkflowTypeId").val())
+    };
+
+    var $infoTag = $('#visit-user-info');
+    var user = {
+      name: $infoTag.attr('data-username'),
+      emailAddress: $infoTag.attr('data-email-address')
+    }
+    Backbone.incident = incident;
+    Backbone.user = user;
+  }
+
+  function route() {
+    var href = window.location.href.toLowerCase();
+
+    if (href.indexOf('postmortem') >= 0) {
+      var postMortem = new PostMortemView();
+      postMortem.render();
+    } else if (href.indexOf('participant') >= 0) {
+      var workflows = new WorkflowsView();
+      workflows.render();
+    }
+  }
 
   var initialize = function () {
-    var postMortemView = new PostMortemView();
-    postMortemView.render();
+    init();
+    route();
   };
+
+  
 
   return {
     initialize: initialize
