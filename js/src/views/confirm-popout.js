0a49451faf1c0b4d7ebd815938ba153bdc4a7382
Zesheng Zong <zesheng.zong@gmail.com>
2016-06-16 14:42:10 -0400

    Add duration param for functions of PageAlert

---
+++ js/src/views/confirm-popout.js
@@ -0,0 +1,45 @@
+﻿define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!templates/confirm-popout-template.html'
+], function ($, _, Backbone, ConfirmPopoutTemplate) {
+
+  var ConfirmPopoutView = Backbone.View.extend({
+
+    el: '#confirm-popout-container',
+
+    attributes: {
+      expand: false
+    },
+
+    events: {
+      'click #popout-cancel-btn': 'cancel',
+      'click #popout-confirm-btn': 'confirm',
+    },
+
+    initialize: function () {
+    },
+
+    render: function () {
+      var template = _.template(ConfirmPopoutTemplate);
+      this.$el.html(template(this.model));
+      this.$el.fadeIn('fast');
+    },
+
+    confirm: function (e) {
+      $(e.target).attr('disabled', 'true');
+      var model = this.model,
+          cb = model.submitCallback,
+          context = model.context;
+      if (typeof cb === 'function') cb.call(context);
+      this.$el.fadeOut('fast');
+    },
+
+    cancel: function () {
+      this.$el.fadeOut('fast');
+    }
+  });
+
+  return ConfirmPopoutView;
+});
